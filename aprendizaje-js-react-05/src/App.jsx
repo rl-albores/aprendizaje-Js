68cf74fdf735cba7a172d42bfc0734b7c9c1a17f
albores-rl <127965480+albores-rl@users.noreply.github.com>
2023-08-23 17:08:12 -0600

    hecho este ejercicio sobre visualizacion de imagen

---
+++ aprendizaje-js-react-05/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Image from './components/image'
+
+function App() {
+  return (
+    <Image
+      src="https://unsplash.com/es/fotos/un-hombre-caminando-por-una-calle-sosteniendo-un-paraguas-1pdbZPoq7uA"
+      alt="📷 by masahiro miyagi"
+    />
+  )
+}
+
+export default App
